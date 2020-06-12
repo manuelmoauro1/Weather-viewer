4d1378d36b5381257ab1089726d7eaea45bf4507
Manuel Moauro <manuelmoauro1@gmail.com>
2020-06-11 22:10:57 -0300

    Seven days forecast added

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
   "use strict";
   document.getElementById("obtener").addEventListener("click", obtenerSegunInput);
-  const userWeather = document.getElementById("card-body");
   userLocation();
 
+  // All the lines below are to get te current weather, with the user location and with the input.
   async function obtenerSegunUbicacion(latitude, longitude) {
     const url = "http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&appid=667de518308564633d6ab9389577a4bf&units=metric";
 
@@ -13,50 +13,26 @@ document.addEventListener("DOMContentLoaded", function () {
         document.getElementById("hidden").classList.remove("hidden");
         document.getElementById("hidden").classList.add("wrapper");
         let json = await response.json();
-        document.getElementById("imgWeather").src = "";
-        if (json.weather[0].description == "clear sky") {
-          document.getElementById("imgWeather").src = "http://openweathermap.org/img/wn/01d@4x.png";
-        } else if (json.weather[0].description == "overcast clouds") {
-          document.getElementById("imgWeather").src = "http://openweathermap.org/img/wn/04d@4x.png";
-        } else if (json.weather[0].description == "mist") {
-          document.getElementById("imgWeather").src = "http://openweathermap.org/img/wn/50d@4x.png";
-        } else if (json.weather[0].description == "broken clouds") {
-          document.getElementById("imgWeather").src = "http://openweathermap.org/img/wn/04d@4x.png";
-        } else if (json.weather[0].description == "few clouds") {
-          document.getElementById("imgWeather").src = "http://openweathermap.org/img/wn/02d@4x.png";
-        } else if (json.weather[0].description == "light intensity drizzle rain") {
-          document.getElementById("imgWeather").src = "images/lightintensity.png";
-        } else if (json.weather[0].description == "scattered clouds") {
-          document.getElementById("imgWeather").src = "http://openweathermap.org/img/wn/03d@4x.png";
-        } else if(json.weather[0].main == "Rain") {
-          document.getElementById("imgWeather").src = "http://openweathermap.org/img/wn/10d@4x.png";
-        } else if(json.weather[0].main == "Thunderstorm") {
-          document.getElementById("imgWeather").src = "http://openweathermap.org/img/wn/011d@4x.png";
-        } else if(json.weather[0].main == "Snow") {
-          document.getElementById("imgWeather").src = "http://openweathermap.org/img/wn/013d@4x.png";
-        } else if(json.weather.main == "Drizzle") {
-          document.getElementById("imgWeather").src = "http://openweathermap.org/img/wn/09d@4x.png"
-        }
 
-        document.getElementById("title").innerHTML =
-          json.name + ", " + json.sys.country;
-        document.getElementById("temperature").innerHTML =
-          json.main.temp + "°C";
-        document.getElementById("thermalSensation").innerHTML =
-          "TS " + json.main.feels_like + "°C";
-        document.getElementById("liteDescription").innerHTML =
-          json.weather[0].description;
-        document.getElementById("pressure").innerHTML =
-          json.main.pressure + " Hpa";
-        document.getElementById("humidity").innerHTML =
-          json.main.humidity + "%";
-        document.getElementById("wind").innerHTML =
-          json.wind.deg + "°, " + json.wind.speed + "km/h";
+        document.getElementById("imgWeather").src = "http://openweathermap.org/img/wn/"+json.weather[0].icon+"@4x.png";
+        document.getElementById("title").innerHTML = json.name + ", " + json.sys.country;
+        document.getElementById("temperature").innerHTML = json.main.temp + "°C";
+        document.getElementById("thermalSensation").innerHTML = "TS " + json.main.feels_like + "°C";
+        document.getElementById("liteDescription").innerHTML = json.weather[0].description;
+        document.getElementById("pressure").innerHTML = json.main.pressure + " Hpa";
+        document.getElementById("humidity").innerHTML = json.main.humidity + "%";
+        document.getElementById("wind").innerHTML = json.wind.deg + "°, " + json.wind.speed + "km/h";
+
+        let latitude = json.coord.lat;
+        let longitude = json.coord.lon;
+        fiveDaysWeather(latitude, longitude);
       }
-    } catch (error) {
+    }
+    catch (error) {
       console.log(error);
     }
   }
+  // Get user location coords.
   function userLocation() {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(function (position) {
@@ -74,30 +50,8 @@ document.addEventListener("DOMContentLoaded", function () {
       let response = await fetch(url);
       if (response.ok) {
         let json = await response.json();
-        document.getElementById("imgWeather").src = "";
-        if (json.weather[0].description == "clear sky") {
-          document.getElementById("imgWeather").src = "http://openweathermap.org/img/wn/01d@4x.png";
-        } else if (json.weather[0].description == "overcast clouds") {
-          document.getElementById("imgWeather").src = "http://openweathermap.org/img/wn/04d@4x.png";
-        } else if (json.weather[0].description == "mist") {
-          document.getElementById("imgWeather").src = "http://openweathermap.org/img/wn/50d@4x.png";
-        } else if (json.weather[0].description == "broken clouds") {
-          document.getElementById("imgWeather").src = "http://openweathermap.org/img/wn/04d@4x.png";
-        } else if (json.weather[0].description == "few clouds") {
-          document.getElementById("imgWeather").src = "http://openweathermap.org/img/wn/02d@4x.png";
-        } else if (json.weather[0].description == "light intensity drizzle rain") {
-          document.getElementById("imgWeather").src = "images/lightintensity.png";
-        } else if (json.weather[0].description == "scattered clouds") {
-          document.getElementById("imgWeather").src = "http://openweathermap.org/img/wn/03d@4x.png";
-        } else if(json.weather[0].main == "Rain") {
-          document.getElementById("imgWeather").src = "http://openweathermap.org/img/wn/10d@4x.png";
-        } else if(json.weather[0].main == "Thunderstorm") {
-          document.getElementById("imgWeather").src = "http://openweathermap.org/img/wn/011d@4x.png";
-        } else if(json.weather[0].main == "Snow") {
-          document.getElementById("imgWeather").src = "http://openweathermap.org/img/wn/013d@4x.png";
-        } else if(json.weather.main == "Drizzle") {
-          document.getElementById("imgWeather").src = "http://openweathermap.org/img/wn/09d@4x.png"
-        }
+
+        document.getElementById("imgWeather").src = "http://openweathermap.org/img/wn/"+json.weather[0].icon+"@4x.png";
         document.getElementById("title").innerHTML = json.name + ", " + json.sys.country;
         document.getElementById("temperature").innerHTML = json.main.temp + "°C";
         document.getElementById("thermalSensation").innerHTML = "TS " + json.main.feels_like + "°C";
@@ -105,8 +59,38 @@ document.addEventListener("DOMContentLoaded", function () {
         document.getElementById("pressure").innerHTML = json.main.pressure + " Hpa";
         document.getElementById("humidity").innerHTML = json.main.humidity + "%";
         document.getElementById("wind").innerHTML = json.wind.deg + "°, " + json.wind.speed + "km/h";
+
+        let latitude = json.coord.lat;
+        let longitude = json.coord.lon;
+        fiveDaysWeather(latitude, longitude);
       }
-    } catch (error) {
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }
+  // I will get the weather for the next 7 days
+  async function fiveDaysWeather (latitude, longitude) {
+    const url = "https://api.openweathermap.org/data/2.5/onecall?lat="+latitude+"&lon="+longitude+"&appid=667de518308564633d6ab9389577a4bf&units=metric";
+
+    try {
+      let response = await fetch(url);
+      if (response.ok) {
+        let json = await response.json();
+
+        for (let i = 1; i < 8; i++) {
+          document.getElementById("sevenImg"+i).src = "http://openweathermap.org/img/wn/"+json.daily[i].weather[0].icon+"@4x.png";
+          document.getElementById("sevenImg"+i).classList.add("imgSeven");
+          document.getElementById("maxT"+i).innerHTML = "Max: "+json.daily[i].temp.max + "°C";
+          document.getElementById("maxT"+i).classList.add("maxT");
+          document.getElementById("minT"+i).innerHTML = "Min: "+json.daily[i].temp.min + "°C";
+          document.getElementById("minT"+i).classList.add("minT");
+          document.getElementById("liteDescriptionSeven"+i).innerHTML = json.daily[i].weather[0].description;
+          document.getElementById("humiditySeven"+i).innerHTML = "Hum:<br>"+json.daily[i].humidity + "%";
+        }
+      }
+    }
+    catch (error) {
       console.log(error);
     }
   }
